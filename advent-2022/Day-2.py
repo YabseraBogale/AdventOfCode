d9418f879cb3e92ed1e6768c4965049fecf76f65
YabseraBogale <yabserabogalercd30932013@gmail.com>
2023-12-04 19:16:17 +0300

    ok

--- advent-2022/Day-2.py
+++ advent-2022/Day-2.py
@@ -4,21 +4,22 @@ file=open('Day-2.txt')
 count=0
 for i in file.readlines():
     value=i.split('\n')[0]
+    num=0
     if(value[0] =="A" and value[1]=="Y"):
-        pass
-    elif(value[0] =="A" and value[1]=="Y"):
+        num=8
+    elif(value[0] =="A" and value[1]=="X"):
         pass  
-    elif(value[0] =="A" and value[1]=="Y"):
-        pass  
-    elif(value[0] =="B" and value[1]=="Y"):
+    elif(value[0] =="A" and value[1]=="Z"):
         pass  
     elif(value[0] =="B" and value[1]=="Y"):
         pass  
-    elif(value[0] =="B" and value[1]=="Y"):
+    elif(value[0] =="B" and value[1]=="X"):
+        pass  
+    elif(value[0] =="B" and value[1]=="Z"):
         pass  
     elif(value[0] =="C" and value[1]=="Y"):
         pass
-    elif(value[0] =="C" and value[1]=="Y"):
+    elif(value[0] =="C" and value[1]=="X"):
         pass  
-    elif(value[0] =="C" and value[1]=="Y"):
+    elif(value[0] =="C" and value[1]=="Z"):
         pass       
